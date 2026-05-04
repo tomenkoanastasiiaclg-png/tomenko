70c35cd1869678d4b6fb147752f214dd3335b543
tomenkoanastasiiaclg-png <tomenko.anastasiia.clg@chnu.edu.ua>
2026-05-04 20:34:54 +0300

    Create Лр 8,1.cpp

---
+++ Лр 8,1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int SumRange(int A, int B)
+{
+    if (A >= B)
+        return 0;
+
+    int sum = 0;
+
+    for (int i = A; i <= B; i++)
+        sum += i;
+
+    return sum;
+}
+
+int main()
+{
+    int A = 12;
+    int B = 15;
+
+    cout << "Sum = " << SumRange(A, B);
+
+    return 0;
+}
